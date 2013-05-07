5724a591708c4242ee8d085e0889bb6c56e7b349
Douglas North Cook <dougnorthcook@gmail.com>
2013-05-07 09:22:15 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Happcity::Application.config.secret_token = '63fc87e4cf1208f48e78df8f4aaf451e01b83bda7b7da6830b572489e58e0923e43ec543eacf4ac64a52e2bd5d22a4cb27a9eb90d36cdcb18e7f1eed20334a7f'
